c02b852cfa0dce976cc4e2a55c8846e94c0dec61
Talib2509 <huseynovmirtalib28@gmail.com>
2024-06-26 19:10:47 +0400

    renetecar

---
+++ rente3car/Program.cs
@@ -0,0 +1,79 @@
+﻿using rente3car;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        RentCar rentCar = new RentCar();
+        Console.WriteLine("Welcome");
+
+        while (true)
+        {
+            Console.WriteLine("1. Add car");
+            Console.WriteLine("2. Remove car");
+            Console.WriteLine("3. Add customer");
+            Console.WriteLine("4. Remove customer");
+            Console.WriteLine("5. Rent car");
+            Console.WriteLine("6. Return car");
+            Console.WriteLine("0. Exit");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter car ID:");
+                    int banid = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter car name:");
+                    string carName = Console.ReadLine();
+                    car car = new car { BanId = banid, CarName = carName };
+                    rentCar.AddCar(car);
+                    break;
+
+                case 2:
+                    Console.WriteLine("Enter car ID to remove:");
+                    int removeCarId = Convert.ToInt32(Console.ReadLine());
+                    rentCar.RemoveCar(removeCarId);
+                    break;
+
+                case 3:
+                    Console.WriteLine("Enter customer ID:");
+                    int customerId = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter customer name:");
+                    string customerName = Console.ReadLine();
+                    Customer customer = new Customer { CustomerId = customerId, Name = customerName };
+                    rentCar.AddCustomer(customer);
+                    break;
+
+                case 4:
+                    Console.WriteLine("Enter customer ID to remove:");
+                    int removeCustomerId = Convert.ToInt32(Console.ReadLine());
+                    rentCar.RemoveCustomer(removeCustomerId);
+                    break;
+
+                case 5:
+                    Console.WriteLine("Enter customer ID:");
+                    int rentCustomerId = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter car ID to rent:");
+                    int rentCarId = Convert.ToInt32(Console.ReadLine());
+                    rentCar.RentACar(rentCustomerId, rentCarId);
+                    break;
+
+                case 6:
+                    Console.WriteLine("Enter customer ID:");
+                    int returnCustomerId = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter car ID to return:");
+                    int returnCarId = Convert.ToInt32(Console.ReadLine());
+                    rentCar.ReturnCar(returnCustomerId, returnCarId);
+                    break;
+
+                case 0:
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
+        }
+    }
+}
---
+++ rente3car/car.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.ConstrainedExecution;
+
+namespace rente3car
+{
+    internal class car
+    {
+        public string CarName { get; set; }
+        public int BanId { get; set; }
+        public bool IsRented { get; set; }
+
+        public car()
+        {
+            IsRented = false;
+        }
+    }
+
+    internal class Customer
+    {
+        public string Name { get; set; }
+        public int CustomerId { get; set; }
+        public int CarCount { get; set; }
+        public car[] RentedCars { get; set; }
+
+        public Customer()
+        {
+            RentedCars = new car[3];
+            CarCount = 0;
+        }
+    }
+
+    internal class RentCar
+    {
+        public int CarCount { get; set; }
+        public int CustomerCount { get; set; }
+        public car[] Cars { get; set; }
+        public Customer[] Customers { get; set; }
+
+        public RentCar()
+        {
+            CarCount = 0;
+            Cars = new car[50];
+            Customers = new Customer[100];
+            CustomerCount = 0;
+        }
+
+        public void AddCar(car car)
+        {
+            if (CarCount < 50)
+            {
+                Cars[CarCount] = car;
+                CarCount++;
+            }
+            else
+            {
+                Console.WriteLine("Cannot add more than 50 cars.");
+            }
+        }
+
+        public void RemoveCar(int banID)
+        {
+            int count = 0;
+            foreach (var item in Cars)
+            {
+                if (item.BanId == banID)
+                {
+                    CarCount--;
+                    Cars[CarCount-1] = null;
+                    break;
+                }
+                count++;
+            }
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            if (CustomerCount < 100)
+            {
+                Customers[CustomerCount] = customer;
+                CustomerCount++;
+            }
+            else
+            {
+                Console.WriteLine("Cannot add more than 100 customers.");
+            }
+        }
+
+        public void RemoveCustomer(int customerID)
+        {
+           
+            int count = 0;
+            foreach (var item in Customers)
+            {
+                if (item.CustomerId == customerID)
+                {
+
+                    Cars[count] = null;
+                    break;
+                }
+                count++;
+
+            }
+        }
+        public void RentACar(int customerID, int banID)
+        {
+            car car = new car();
+            bool check = true;
+            foreach (var item in Cars)
+            {
+                if (item.BanId == banID)
+                {
+                    car = item;
+                    item.IsRented = true;
+                    check = false;
+                    break;
+                }
+            }
+            if (check)
+            {
+
+                Console.WriteLine("gdgfg");
+            }
+            else
+            {
+                check = true;
+                int ecount = 0;
+                foreach (var item in Customers)
+                {
+                    ecount++;
+                    if (item.CustomerId == customerID)
+                    {
+                        if (item.CarCount < 3)
+                        {
+                            item.CarCount++;
+                            item.RentedCars[ecount] = car;
+                            check = false;
+                            break;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        public void ReturnCar(int customerID, int banID)
+        {
+            car car = new car();
+            bool check = true;
+            foreach (var item in Cars)
+            {
+                if (item.BanId == banID)
+                {
+                    car = item;
+                    item.IsRented = false;
+                    check = false;
+                    break;
+                }
+            }
+            if (check)
+            {
+
+                Console.WriteLine("gdgfg");
+            }
+            else
+            {
+                check = true;
+                int ecount = 0;
+                foreach (var item in Customers)
+                {
+                    ecount++;
+                    if (item.CustomerId == customerID)
+                    {
+                        item.CarCount--;
+                        item.RentedCars[ecount] = null;
+                        check = false; break;
+                    }
+                }
+            }
+        }
+    }
+}
